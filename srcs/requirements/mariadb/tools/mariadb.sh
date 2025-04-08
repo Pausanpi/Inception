10da1bf305efcb0b874bc88aed88d5cf8ca0ed23
Paula Sánchez <123211878+Pausanpi@users.noreply.github.com>
2025-04-08 11:27:59 +0200

    Add files via upload

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,11 +1,36 @@
-echo "CREATE DATABASE IF NOT EXISTS ${MARIA_DATABASE};" >> init.sql
-echo "CREATE USER '${MARIA_USER}'@'%' IDENTIFIED BY '${MARIA_PASS}';" >> init.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIA_ROOT_PASS}';" >> init.sql
-echo "GRANT ALL PRIVILEGES ON *.* TO '${MARIA_USER}'@'%';" >> init.sql
-echo "FLUSH PRIVILEGES;" >> init.sql
+#!/bin/bash
 
-chmod 777 init.sql
-mv init.sql /run/mysqld/init.sql
-chown -R mysql:root /var/run/mysqld
+# Modificar la configuración de MariaDB para aceptar conexiones remotas
+sed -i 's/bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
 
-mariadbd --init-file /run/mysqld/init.sql
+# Iniciar directamente mysqld_safe para crear bases de datos
+if [ ! -d "/var/lib/mysql/$MARIA_DATABASE" ]; then
+    echo "Iniciando MariaDB temporalmente..."
+    mysqld_safe --skip-networking &
+    
+    # Esperar a que MariaDB esté listo
+    until mysqladmin ping -h localhost --silent; do
+        echo "Esperando a que MariaDB esté disponible..."
+        sleep 2
+    done
+    
+    echo "Creando base de datos y usuario..."
+    mysql -u root <<EOF
+CREATE DATABASE IF NOT EXISTS $MARIA_DATABASE;
+CREATE USER IF NOT EXISTS '$MARIA_USER'@'%' IDENTIFIED BY '$MARIA_PASS';
+GRANT ALL PRIVILEGES ON $MARIA_DATABASE.* TO '$MARIA_USER'@'%';
+FLUSH PRIVILEGES;
+EOF
+    
+    echo "Cerrando MariaDB para reiniciarlo con la nueva configuración..."
+    mysqladmin -u root shutdown
+    
+    # Asegurar que el proceso de mysqld_safe termine completamente
+    while pgrep -f mysqld > /dev/null; do
+        echo "Esperando a que MariaDB se cierre..."
+        sleep 1
+    done
+fi
+
+echo "Iniciando MariaDB con la configuración actualizada..."
+exec mysqld_safe
--- srcs/requirements/nginx/tools/nginx.sh
+++ srcs/requirements/nginx/tools/nginx.sh
@@ -1,11 +1,8 @@
-#!/bin/sh
-
+#!/bin/bash
 mkdir -p /etc/nginx/ssl
-
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -out "$CERTIFICATE" -keyout "$CERT_KEY" -subj "/C=ES/ST=Andalucia/L=Malaga/O=42/OU=42/CN=$DOMINIO_NAME/UID=$LOGIN"
-
-sed -i "s/dominio_name/$DOMINIO_NAME/" /etc/nginx/nginx.conf
-sed -i "s#ssl_certificate /etc/nginx/ssl/nginx-selfsigned.crt;#ssl_certificate $CERTIFICATE;#" /etc/nginx/nginx.conf
-sed -i "s#ssl_certificate_key /etc/nginx/ssl/nginx-selfsigned.key;#ssl_certificate_key $CERT_KEY;#" /etc/nginx/nginx.conf
-
-nginx -g "daemon off;"
+if [ ! -f /etc/nginx/ssl/nginx-selfsigned.crt ]; then
+    echo "Nginx: setting up ssl ...";
+    openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout /etc/nginx/ssl/nginx-selfsigned.key -out /etc/nginx/ssl/nginx-selfsigned.crt -subj "/C=ES/ST=Malaga/L=Malaga/O=wordpress/CN=pausanch.42.fr";
+    echo "Nginx: ssl is set up!";
+fi
+exec "$@"
