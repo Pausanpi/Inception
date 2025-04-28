ef538d28ff1dc68eb5b873264add6b1e05e165ee
Paula Sánchez <123211878+Pausanpi@users.noreply.github.com>
2025-04-28 09:56:23 +0200

    Delete srcs directory

--- srcs/requirements/nginx/tools/nginx.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mkdir -p /etc/nginx/ssl
-if [ ! -f /etc/nginx/ssl/nginx-selfsigned.crt ]; then
-    echo "Nginx: setting up ssl ...";
-    openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout /etc/nginx/ssl/nginx-selfsigned.key -out /etc/nginx/ssl/nginx-selfsigned.crt -subj "/C=ES/ST=Malaga/L=Malaga/O=wordpress/CN=pausanch.42.fr";
-    echo "Nginx: ssl is set up!";
-fi
-exec "$@"
